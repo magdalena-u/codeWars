f4a60703d4c6c4f0356198f9ad1a69e9228921be
magda-u <magdalena.urzon@gmail.com>
2019-05-06 17:05:48 +0200

    kata Equal Sides Of An Array

--- equalSides.js
+++ equalSides.js
@@ -1,11 +1,6 @@
 function findEvenIndex(arr) {
   let result;
   for (let n = 1; n <= arr.length; n++) {
-    console.log(arr.slice(0, n - 1).reduce((prev, next) => prev + next, 0));
-    console.log(
-      arr.slice(n, arr.length).reduce((prev, next) => prev + next, 0)
-    );
-    console.log("ups");
     if (
       arr.slice(0, n - 1).reduce((prev, next) => prev + next, 0) ===
       arr.slice(n, arr.length).reduce((prev, next) => prev + next, 0)
